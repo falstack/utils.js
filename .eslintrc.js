78004c499d481a38e3a6b800f27b57bf581ec32e
falstack <icesilt@outlook.com>
2017-09-07 21:06:56 +0800

    add: README.md

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,5 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: 'standard',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  rules: {},
-  globals: {}
+  extends: 'standard'
 }
